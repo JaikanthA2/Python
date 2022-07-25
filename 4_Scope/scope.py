d248010c2d9aefa655ee3f01a406acf9b9944801
Ubuntu <ubuntu@ip-172-31-8-88.us-west-1.compute.internal>
2022-07-25 11:56:06 +0000

    Python scope

---
+++ 4_Scope/scope.py
@@ -0,0 +1,13 @@
+calculation_to_units= 24
+name_of_unit="hours"
+
+def scope_check(num_of_days):
+    print ("=====================================")
+    my_var="variable inside the function"
+    print (name_of_unit)
+    print (num_of_days)
+    print (my_var)
+    print("=======================================")
+
+
+scope_check(30)
