adbd313f53a6b8e0b39ca4b0337a0a42e95420ad
Ubuntu <ubuntu@ip-172-31-8-88.us-west-1.compute.internal>
2022-08-08 06:54:34 +0000

    dding user input dir

---
+++ 5_User_Input/user_input.py
@@ -0,0 +1,2 @@
+user_input= input("Enter user input \n")
+print ("The user input is "+ user_input)
---
+++ 6_Function_with_return_value/functionwithreturnvalue.py
@@ -0,0 +1,15 @@
+calculation_to_units= 24
+name_of_unit="hours"
+
+user_input= input("Enter user input number of days\n")
+
+
+print (user_input)
+def days_to_units():
+    return print (f"{user_input} days are {user_input} {name_of_unit}")
+
+my_result= days_to_units()
+print (my_result)
+
+
+# The return keyword is placed in the function which return the value and store in the varible my_result and print 
