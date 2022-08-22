1cd301fda58cc8d5ee3010e9bafee0a2a536503c
Ubuntu <ubuntu@ip-172-31-8-88.us-west-1.compute.internal>
2022-08-22 10:34:52 +0000

    7_Conditional_and_Boolean added

--- 6_Function_with_return_value/functionwithreturnvalue.py
+++ 6_Function_with_return_value/functionwithreturnvalue.py
@@ -2,11 +2,11 @@ calculation_to_units= 24
 name_of_unit="hours"
 
 user_input= input("Enter user input number of days\n")
-
+user_input_number=int(user_input)
 
 print (user_input)
 def days_to_units():
-    return print (f"{user_input} days are {user_input} {name_of_unit}")
+    return  (f"{user_input_number} days are  {user_input_number * 24} {name_of_unit}")
 
 my_result= days_to_units()
 print (my_result)
