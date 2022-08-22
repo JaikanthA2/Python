f9b5b5394426050f44660ad5bc0895b43be8bf16
Ubuntu <ubuntu@ip-172-31-8-88.us-west-1.compute.internal>
2022-08-22 12:24:46 +0000

    Modifed the file

--- 7_Conditional_and_Boolean/conditionalandboolean.py
+++ 7_Conditional_and_Boolean/conditionalandboolean.py
@@ -1,20 +1,31 @@
 calculation_to_units= 24
 name_of_unit="hours"
 
-user_input= input("Enter user input number of days\n")
-user_input_number=int(user_input)
 
-print (user_input_number)
+
+
+
 
 
 def days_to_units(user_input_number):
-    if user_input_number > 0 and type(user_input_number) == int :
+    condition_check = user_input_number > 0
+    print(type(condition_check))
+    condition_datatype= type(user_input_number)
+    if user_input_number > 0:
         return (f"{user_input_number} days are  {user_input_number * 24} {name_of_unit}")
+    elif user_input_number == 0:
+      return (f"{user_input_number} is not valid")
     else:
         return (f"{user_input_number} Invalid entry")
-my_result= days_to_units(user_input_number)
-print (my_result)
 
+user_input= input("Enter user input number of days\n")
 
+if user_input.isdigit():
+  user_input_number=int(user_input)
+  my_result= days_to_units(user_input_number)
+  print (my_result)
+
+else:
+   print("Input is not a number")
 
 # The return keyword is placed in the function which return the value and store in the varible my_result and print 
