ccad5598b0a4a2db7a20b860c20b35ec5f60cc97
Ubuntu <ubuntu@ip-172-31-8-88.us-west-1.compute.internal>
2022-08-23 10:41:18 +0000

    Dicitionary datatype code pushed

---
+++ 12_BuiltinFunction/builtinfunction.py
@@ -0,0 +1,9 @@
+""" There are many bulit in function and some of them we already used in the concept while learing"""
+
+print("One of the bulit in function")
+input ("Some text")
+set ([1,2,3])
+int ("20")
+"2, 3".split()
+[1, 2, 3].count()
+
